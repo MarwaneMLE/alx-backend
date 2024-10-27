f88554e89a49ff211bcc3fc3ca71e1fa1d788cdd
Marwane <khmarwane10@gmail.com>
2024-10-27 21:42:40 +0000

    tasks 0 & 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,6 +6,7 @@ from typing import Tuple, List
 import math
 import csv
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Retrieves the index range from a given page and page size."""
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
